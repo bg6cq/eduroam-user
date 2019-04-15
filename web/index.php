c5b34dd536a08d969471d65f386c2edaf2eadcd4
Zhang Huanjie <james@ustc.edu.cn>
2019-04-15 11:38:51 +0800

    remove fsyy

--- web/index.php
+++ web/index.php
@@ -78,7 +78,7 @@ $cmd = safe_get("cmd");
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=8.0"/>
 <link href="table.css" type="text/css" rel="stylesheet" /> 
-<title>fsyy.ustc.edu.cn eduroam用户管理</title>
+<title>eduroam用户管理</title>
 </head>
 <body>
 
